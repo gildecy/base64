3ebb58fbeaf4366a416633dbe95ae75cefa77c88
Gildecy de Jesus Sá <101933897+gildecy@users.noreply.github.com>
2024-06-09 17:38:23 -0300

    Update base64.js

--- base64.js
+++ base64.js
@@ -1,33 +1,32 @@
  <script>
-        file.addEventListener('change',previewFile);
-       download.addEventListener('click',downloadFile);
-       output.addEventListener('input',previewText);
+ const fileInput = document.getElementById('file');
+        const output = document.getElementById('output');
+        const preview = document.getElementById('preview');
+        const downloadButton = document.getElementById('download');
 
+        fileInput.addEventListener('change', previewFile);
+        downloadButton.addEventListener('click', downloadFile);
+        output.addEventListener('input', previewText);
 
-function previewFile({target}){
+        function previewFile(event) {
+            const file = event.target.files[0];
+            const reader = new FileReader();
 
-    const file = target.file[0];
-    const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                output.value = reader.result;
+                preview.src = reader.result;
+            };
+        }
 
-    reader.readAsDataURL(file);
+        function downloadFile() {
+            const link = document.createElement('a');
+            link.href = output.value;
+            link.download = 'image.png';
+            link.click();
+        }
 
-    reader.onload=()=> {
-        output.value = reader.result;
-        preview.src = reader.result;
-    }
-
-};
-
-function downloadFile (){
-    const link = document.createElement('a');
-     link.href = output.value;
-     link.download ='image.png';
-     link.click();
-}
-
-
-function previewText({target}) {
-    let base64 = target.value.replace(/^data:image\/[a-z]+;base64,/,"");
-    preview.src = `data:image/png;base64,${base64}`;
-}
+        function previewText(event) {
+            const base64 = event.target.value.replace(/^data:image\/[a-z]+;base64,/, "");
+            preview.src = `data:image/png;base64,${base64}`;
     </script>
